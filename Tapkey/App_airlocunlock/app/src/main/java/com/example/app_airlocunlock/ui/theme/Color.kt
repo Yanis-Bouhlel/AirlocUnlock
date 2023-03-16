9fb68ba5da9c9d8c65cbc07e6dbb10641d696f38
Yanis-Bouhlel <91103329+Yanis-Bouhlel@users.noreply.github.com>
2023-03-16 23:50:55 +0100

    11

--- Tapkey/App_airlocunlock/app/src/main/java/com/example/app_airlocunlock/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.app_airlocunlock.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFFF5A60)
-val Purple500 = Color(0xFFFF5A60)
-val Purple700 = Color(0xFFFF5A60)
-val Teal200 = Color(0xFFFF5A60)
-val White = Color(0xFFFFFFFFF)
