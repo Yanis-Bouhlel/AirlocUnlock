de0b7b4b9923f6fde344d0c09e9ad843946606d4
Yanis Bouhlel <yanis.bouhlel@lycee-jeanrostand.fr>
2023-04-12 10:14:59 +0200

    staive

--- Tapkey/Serrure/Code/V4/Motor/Motor.cpp
+++ Tapkey/Serrure/Code/V4/Motor/Motor.cpp
@@ -1,11 +1,9 @@
 //
 // Created by bouhlel_y2 on 4/6/2023.
 //
-
 #include "Motor.h"
-
-Motor::Motor() {
-
+Motor::Motor(int pSpeed, int pNbPas, int p1,int p2, int p3, int p4) {
+  setup(pSpeed, pNbPas, p1, p2, p3, p4);
 }
 
 void Motor::setup(int pSpeed, int pNbPas, int p1,int p2, int p3, int p4) {
@@ -15,8 +13,8 @@ void Motor::setup(int pSpeed, int pNbPas, int p1,int p2, int p3, int p4) {
     this->pin4 = p4;
     this->nbPas = pNbPas ;
     this->speed = pSpeed ;
-    motor(this->nbPas, this->pin1, this->pin2,this->pin3, this->pin4);
-    motor.setSpeed(this->speed);
+    motor = new Stepper(this->nbPas, this->pin1, this->pin2, this->pin3, this->pin4); // création d'une instance de Stepper
+    motor->setSpeed(this->speed);
 }
 
 void Motor::getPin(int p1,int p2, int p3, int p4) {
@@ -27,6 +25,6 @@ void Motor::getPin(int p1,int p2, int p3, int p4) {
 }
 
 void Motor::move(int pMove) {
-    //setup(this->speed, this->nbPas, this->pin1, this->pin2,this->pin3, this->pin4);
-    motor.step(pMove) ;
+    setup(this->speed, this->nbPas, this->pin1, this->pin2,this->pin3, this->pin4);
+    motor->step(pMove) ;
 }
--- Tapkey/Serrure/Code/V4/Motor/Motor.h
+++ Tapkey/Serrure/Code/V4/Motor/Motor.h
@@ -4,6 +4,8 @@
 
 #ifndef V4_MOTOR_H
 #define V4_MOTOR_H
+#include <Stepper.h>
+#include "Arduino.h"
 
 
 class Motor {
@@ -16,7 +18,7 @@ class Motor {
         int pin3 ;
         int pin4 ;
     public:
-        Motor() ;
+        Motor(int pSpeed, int pNbPas, int p1,int p2, int p3, int p4) ;
         void setup(int pSpeed, int pNbPas, int p1,int p2, int p3, int p4);
         void getPin(int p1,int p2, int p3, int p4) ;
         void move(int pMove) ;
