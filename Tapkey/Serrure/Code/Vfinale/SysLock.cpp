0639ae3b0e545406c66b6383bdaf76f376133489
Yanis-Bouhlel <91103329+Yanis-Bouhlel@users.noreply.github.com>
2023-05-30 01:39:20 +0200

    PRojetSite

--- Tapkey/Serrure/Code/Vfinale/SysLock.cpp
+++ Tapkey/Serrure/Code/Vfinale/SysLock.cpp
@@ -24,26 +24,24 @@ void SysLock::Close() {
 
 int SysLock::getBattery() {
 
-    const int pinAnalogique = A0;  // Numéro de la broche analogique utilisée
+    const int pinAnalogique = A0;  
 
-    const float tensionMin = 10.0;  // Tension minimale de la batterie
-    const float tensionMax = 12.0;  // Tension maximale de la batterie
+    const float tensionMin = 10.0; 
+    const float tensionMax = 12.0; 
 
-    const float R1 = 10000.0;  // Valeur de résistance connectée à la tension de la batterie
-    const float R2 = 10000.0;  // Valeur de résistance connectée à la masse (GND)
+    const float R1 = 10000.0;  
+    const float R2 = 10000.0;  
+    
+    int lectureAnalogique = analogRead(pinAnalogique);  
+    float tensionBatterie = (lectureAnalogique / 1023.0) * 5.0 * (R1 + R2) / R2;
+    int niveauBatterie = map(tensionBatterie, tensionMin, tensionMax, 0, 100);  
 
-
-    int lectureAnalogique = analogRead(pinAnalogique);  // Lecture de la valeur analogique
-    float tensionBatterie = (lectureAnalogique / 1023.0) * 5.0 * (R1 + R2) / R2;  // Calcul de la tension de la batterie
-    int niveauBatterie = map(tensionBatterie, tensionMin, tensionMax, 0, 100);  // Calcul du niveau de batterie en pourcentage
-
-    // Utilisation du niveau de batterie (par exemple, l'afficher sur le moniteur série)
     Serial.begin(9600);
     Serial.print("Niveau de batterie : ");
     Serial.print(niveauBatterie);
     Serial.println("%");
 
-    delay(1000);  // Attente d'une seconde avant la prochaine lecture
+    delay(1000); 
 
     this->Battery = niveauBatterie ;
 
